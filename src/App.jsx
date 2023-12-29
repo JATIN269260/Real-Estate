4f69184fe484c7098cb72567a8ecbca45e193948
Jatin Garg <71146552+JATIN269260@users.noreply.github.com>
2023-12-29 12:33:59 +0530

    Create headre component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
     <Route path ="/" element={<Home/> }></Route>
     <Route path ="/sign-in" element={<SignIn/> }></Route>
@@ -15,6 +18,7 @@ export default function App() {
     <Route path ="/about" element={<About/> }></Route>
     <Route path ="/profile" element={<Profile/> }></Route>
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
