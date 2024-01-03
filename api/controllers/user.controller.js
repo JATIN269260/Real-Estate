c7a796c78a6c50414eee2d2c8045fe027f72e1cc
Jatin Garg <71146552+JATIN269260@users.noreply.github.com>
2024-01-03 23:23:29 +0530

    create signup api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
   res.json({
-    message: "Welcome to the API",
+    message: "Api route is working!",
   });
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controllers.js";
+import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", test);
+router.get("/test", test);
 
 export default router;
