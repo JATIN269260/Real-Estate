3731afadebb9b605385693204556c72bad1ff6fc
Jatin Garg <71146552+JATIN269260@users.noreply.github.com>
2024-01-31 19:37:47 +0530

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
@@ -10,12 +10,13 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "You can only update yiour iwn account!"));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-    const updateUser = await User.findByIdAndUpdate(
+
+    const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
         $set: {
@@ -28,8 +29,9 @@ export const updateUser = async (req, res, next) => {
       { new: true }
     );
 
-    const { password, ...rest } = updateUser._doc;
-    res.status | (200).json(rest);
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
 dotenv.config();
 
 mongoose
@@ -15,7 +14,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 const app = express();
 
 app.use(express.json());
@@ -25,13 +23,11 @@ app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Serverv Error";
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
