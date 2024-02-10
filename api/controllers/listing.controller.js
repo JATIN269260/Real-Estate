6fd649f052af785ae2b620cd899ecc2e9265189e
Jatin Garg <71146552+JATIN269260@users.noreply.github.com>
2024-02-10 12:20:38 +0530

    search page ui and api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -57,3 +57,53 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async () => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer == "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished == "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking == "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type == "all") {
+      offer = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchterm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
